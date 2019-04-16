47544c87b05f4278f8c53f271e236ac9390b3ba4
jayevan <jayevan@sina.com>
2019-04-17 01:43:38 +0800

    Add zookeeper consul as discovery server

---
+++ eureka-cluster-server/eureka-server-node1/src/main/java/org/rhine/EurekaServer.java
@@ -0,0 +1,16 @@
+package org.rhine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka server
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class);
+    }
+}
